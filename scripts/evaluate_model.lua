a3d0f7d117618bf6ef64ed3ec61a285870bcc392
Achal Dave <achalddave@live.com>
2016-06-04 01:26:59 -0400

    Use data_loader in evaluation script, save predictions

--- scripts/evaluate_model.lua
+++ scripts/evaluate_model.lua
@@ -1,9 +1,16 @@
+--[[ Evaluate a trained torch model on an LMDB.
+--
+-- Note that this should be run from the root directory, not from within
+-- scripts/.
+--]]
+
 package.path = package.path .. ";../?.lua"
 
 local argparse = require 'argparse'
 local lmdb = require 'lmdb'
 local torch = require 'torch'
 
+local DataLoader = require('data_loader').DataLoader
 local evaluator = require 'evaluator'
 local video_frame_proto = require 'video_util.video_frames_pb'
 
@@ -13,13 +20,15 @@ local parser = argparse() {
 parser:argument('model', 'Model file.')
 parser:argument('labeled_video_frames_lmdb',
                 'LMDB containing LabeledVideoFrames to evaluate.')
+parser:argument('labeled_video_frames_without_images_lmdb',
+                'LMDB containing LabeledVideoFrames without images.')
 
 local args = parser:parse()
 
 -- More config variables.
 local NUM_LABELS = 65
-local BATCH_SIZE = 64
-local GPU = 1
+local BATCH_SIZE = 96
+local GPU = 2
 local MEANS = {96.8293, 103.073, 101.662}
 local CROP_SIZE = 224
 
@@ -30,77 +39,86 @@ torch.setdefaulttensortype('torch.FloatTensor')
 local model = torch.load(args.model)
 
 -- Open database.
-local db = lmdb.env { Path = args.labeled_video_frames_lmdb }
-db:open()
-local transaction = db:txn(true --[[readonly]])
-local cursor = transaction:cursor()
-local num_samples = db:stat().entries
+local data_loader = DataLoader(args.labeled_video_frames_lmdb,
+                               args.labeled_video_frames_without_images_lmdb,
+                               NUM_LABELS)
 
 -- Pass each image in the database through the model, collect predictions and
 -- groundtruth.
 local gpu_inputs = torch.CudaTensor()
-local finished = false
 local all_predictions
 local all_labels
 local samples_complete = 0
 -- TODO(achald): Remove this
 while true do
-    if finished then break end
-    local batch_images = {}
-    local batch_labels = {}
-    for i = 1, BATCH_SIZE do
-        local video_frame = video_frame_proto.LabeledVideoFrame()
-        local _, video_frame_proto_tensor = cursor:get()
-        video_frame:ParseFromString(video_frame_proto_tensor:storage():string())
+    if samples_complete == data_loader:num_samples() then
+        break
+    end
+    local to_load = BATCH_SIZE
+    if samples_complete + BATCH_SIZE > data_loader:num_samples() then
+        to_load = data_loader:num_samples() - samples_complete
+    end
+    images_table, labels_table = data_loader:load_batch(to_load)
+    data_loader:fetch_batch_async(to_load)
 
-        local image_proto = video_frame.frame.image
-        local image_storage = torch.ByteStorage()
-        image_storage:string(image_proto.data)
-        local img = torch.ByteTensor(image_storage):reshape(
-            image_proto.channels, image_proto.height, image_proto.width)
+    local images = torch.Tensor(
+        #images_table, images_table[1]:size(1), CROP_SIZE, CROP_SIZE)
+    local labels = torch.ByteTensor(#labels_table, NUM_LABELS)
+    for i, img in ipairs(images_table) do
+        -- Process image after converting to the default Tensor type.
+        -- (Originally, it is a ByteTensor).
+        img = img:typeAs(images)
+        local x_origin = (img:size(3) - CROP_SIZE) / 2
+        local y_origin = (img:size(2) - CROP_SIZE) / 2
+        img = image.crop(img, x_origin, y_origin, x_origin + CROP_SIZE, y_origin + CROP_SIZE)
         for channel = 1, 3 do
             img[{{channel}, {}, {}}]:add(-MEANS[channel])
         end
-        img = image.crop(img, "c" --[[center crop]], CROP_SIZE, CROP_SIZE)
-        table.insert(batch_images, img)
-
-        -- Load labels in an array.
-        local labels = torch.ByteTensor(1, NUM_LABELS):zero()
-        for _, label in ipairs(video_frame.label) do
-            -- Label ids start at 0.
-            labels[{1, label.id + 1}] = 1
-        end
-        table.insert(batch_labels, labels)
-
-        if not cursor:next() then
-            finished = true
-            break
-        end
-        samples_complete = samples_complete + 1
+        images[i] = img
+        labels[i] = data_loader:labels_to_tensor(labels_table[i], NUM_LABELS)
     end
-    gpu_inputs:resize(#batch_images,
-                      batch_images[1]:size(1),
-                      batch_images[1]:size(2),
-                      batch_images[1]:size(3))
+
+    gpu_inputs:resize(images:size()):copy(images)
+
     local predictions = model:forward(gpu_inputs):type(
         torch.getdefaulttensortype())
-    local batch_labels_tensor = torch.cat(batch_labels, 1):type('torch.ByteTensor')
+    labels = labels:type('torch.ByteTensor')
 
     if all_predictions == nil then
         all_predictions = predictions
-        all_labels = batch_labels_tensor
+        all_labels = labels
     else
         all_predictions = torch.cat(all_predictions, predictions, 1)
-        all_labels = torch.cat(all_labels, batch_labels_tensor, 1)
+        all_labels = torch.cat(all_labels, labels, 1)
     end
 
+    local num_labels_with_groundtruth = 0
+    for i = 1, NUM_LABELS do
+        if torch.any(all_labels[{{}, {i}}]) then
+            num_labels_with_groundtruth = num_labels_with_groundtruth + 1
+        end
+    end
+
+    print('Number of labels with groundtruth:', num_labels_with_groundtruth)
     local map_so_far = evaluator.compute_mean_average_precision(
         all_predictions, all_labels)
+    local batch_map = evaluator.compute_mean_average_precision(
+        predictions, labels)
     print(string.format(
-        '%s: Finished %d/%d. mAP so far: %.5f',
-        os.date('%X'), samples_complete, num_samples, map_so_far))
+        '%s: Finished %d/%d. mAP so far: %.5f, batch mAP: %.5f',
+        os.date('%X'), samples_complete, data_loader:num_samples(), map_so_far,
+        batch_map))
     collectgarbage()
+    samples_complete = samples_complete + to_load
 end
+for i = 1, all_predictions:size(2) do
+    local ap = evaluator.compute_mean_average_precision(
+        all_predictions[{{}, {i}}], all_labels[{{}, {i}}])
+    print(string.format('Class %d\t AP: %.5f', i, ap))
+end
+
 local map = evaluator.compute_mean_average_precision(
     all_predictions, all_labels)
 print('mAP: ', map)
+torch.save('evaluation.t7',
+           {predictions = all_predictions, labels = all_labels})
