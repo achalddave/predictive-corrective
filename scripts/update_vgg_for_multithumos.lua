9339b9fe1f916685fa2043ec24cdd2c15d73cc2d
Achal Dave <achalddave@live.com>
2016-09-27 09:32:44 -0400

    Add option to keep last fc layer

--- scripts/update_vgg_for_multithumos.lua
+++ scripts/update_vgg_for_multithumos.lua
@@ -1,6 +1,8 @@
 --[[
-Updates a pre-trained VGG-16 network by chopping off the last layer and adding
-a cross entropy layer.
+Updates a pre-trained VGG-16 network by
+1. Removing the softmax layer
+2. (Optionally) Chopping off the last FC layer
+3. Adding an FC layer with a specified output size.
 ]]--
 
 local argparse = require 'argparse'
@@ -16,16 +18,30 @@ parser:argument('model', 'Torch model')
 parser:argument('num_classes', 'Number of outputs the last layer should have.')
       :convert(tonumber)
 parser:argument('output_model', 'Output MultiTHUMOS model')
-
+parser:flag('--keep_last_fc',
+            'If specified, the last fully connected layer is not removed. ' ..
+            'Instead, one ReLU and one Dropout layer is added ' ..
+            '(in that order), and the new fully connected layer is added on ' ..
+            'top of that.'):default(false)
 local args = parser:parse()
 
 model = torch.load(args.model)
 model:remove() -- Remove softmax layer.
-model:remove() -- Remove FC8 layer.
+if args.keep_last_fc then
+    local rectifiers = model:findModules('cudnn.ReLU')
+    local dropouts = model:findModules('nn.Dropout')
+    -- Add a clone of the last ReLU and Dropout layers.
+    model:add(rectifiers[#rectifiers]:clone())
+    model:add(dropouts[#dropouts]:clone())
+else
+    model:remove() -- Remove FC8 layer.
+end
 
--- Add new FC8 layer with args.num_classes outputs.
-local new_fc8 = nn.Linear(4096, args.num_classes)
-new_fc8.name = 'fc8'
-model:add(new_fc8)
+-- Should be either 4096 (FC7 output) or 1000 (FC8 output)
+local linear_layers = model:findModules('nn.Linear')
+local last_layer_output_size = linear_layers[#linear_layers].weight:size(1)
+-- Add new output layer with args.num_classes outputs.
+local new_output_layer = nn.Linear(last_layer_output_size, args.num_classes)
+model:add(new_output_layer)
 
 torch.save(args.output_model, model)
