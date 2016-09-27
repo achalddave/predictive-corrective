f9ba9f480be12f7f79963c50545fb82c504fde63
Achal Dave <achalddave@live.com>
2016-09-26 23:05:58 -0400

    More general, more compact make_pyramid_model

--- scripts/make_pyramid_model.lua
+++ scripts/make_pyramid_model.lua
@@ -45,6 +45,10 @@ parser:flag('--weighted_merge',
 
 local args = parser:parse()
 
+local MERGE_LAYER_INDICES = {29 --[[conv53]]} -- {22 --[[conv43]], 29 --[[conv53]]}
+local SEQUENCE_LENGTH = 4
+local MERGE_INPUT_LENGTH = 4
+
 local MERGE_OPTIONS = { sum = 1, avg = 2 }
 local WEIGHT_TYPE_OPTIONS = { tied = 1, untied = 2, residual_tied = 3 }
 
@@ -53,8 +57,6 @@ assert(merge_type ~= nil, 'Invalid merge option.')
 
 local weight_type = WEIGHT_TYPE_OPTIONS[args.weight_type]
 assert(weight_type ~= nil, 'Invalid weight type.')
-
-local SEQUENCE_LENGTH = 4
 
 local model = torch.load(args.model)
 if torch.isTypeOf(model, 'nn.DataParallelTable') then
@@ -65,9 +67,6 @@ if torch.isTypeOf(model, 'nn.Sequencer') then
     model = model:get(1):get(1)
 end
 
-local CONV4_3_INDEX = 22 -- HACK!
-local CONV5_3_INDEX = 29 -- HACK!
-
 local function extract_stub(model, start_index, end_index)
     local output_model = nn.Sequential()
     for i = start_index, end_index do
@@ -76,14 +75,16 @@ local function extract_stub(model, start_index, end_index)
     return output_model
 end
 
-local function create_merger(sequence_length, merge_type, weighted)
+local function create_merger(
+    sequence_length, merge_input_length, merge_type, weighted)
     --[[
     -- Create a model that takes in a table of inputs and merge every
     -- consecutive pair output (without overlap).  The number of inputs to the
     -- model is `sequence_length` and the number of outputs from the model is
-    -- `sequence_length / 2`.
+    -- `sequence_length / merge_input_length`.
     --]]
-    assert(sequence_length % 2 == 0, 'Sequence length must be even.')
+    assert(sequence_length % merge_input_length == 0,
+           'Sequence length must be even.')
     weighted = weighted == nil and false or weighted
 
     -- Merge table of inputs
@@ -103,23 +104,22 @@ local function create_merger(sequence_length, merge_type, weighted)
             merging_layer = nn.CAddTable()
         elseif merge_type == MERGE_OPTIONS['avg'] then
             merging_layer = nn.CAvgTable()
-        else
-            error('Invalid merge type:', merge_type)
         end
     end
 
-    -- Merge every 2 model_stub outputs.
-    local pair_mergers = nn.ConcatTable()
-    local num_pairs = sequence_length / 2
-    for pair_index = 1, num_pairs do
-        local left_index = pair_index * 2 - 1 -- Index of 'left' frame in pair.
+    -- Merge every `merge_input_length` model_stub outputs.
+    local mergers = nn.ConcatTable()
+    local num_mergers = sequence_length / merge_input_length
+    for merge_index = 1, num_mergers do
+        -- Index of 'left' frame in pair.
+        local left_index = merge_index * merge_input_length - 1
         local merger = nn.Sequential()
         merger:add(nn.NarrowTable(left_index, 2)) -- Select the current pair.
         merger:add(merging_layer:clone()) -- Merge the current pair.
-        pair_mergers:add(merger)
+        mergers:add(merger)
     end
     local output_model = nn.Sequential()
-    output_model:add(pair_mergers)
+    output_model:add(mergers)
     return output_model
 end
 
@@ -137,73 +137,56 @@ local function reset_weights(model)
     end
 end
 
-local stub_to_conv4_3 = extract_stub(model, 1, CONV4_3_INDEX)
-local parallel_stubs_to_conv4_3 = nn.ParallelTable()
--- For residual weights.
-local reset_stub_to_conv4_3 = stub_to_conv4_3:clone()
-reset_weights(reset_stub_to_conv4_3)
-for i = 1, SEQUENCE_LENGTH do
-    local stub
-    if weight_type == WEIGHT_TYPE_OPTIONS.untied then
-        stub = stub_to_conv4_3:clone()
-    elseif weight_type == WEIGHT_TYPE_OPTIONS.tied then
-        stub = stub_to_conv4_3:sharedClone()
-    elseif weight_type == WEIGHT_TYPE_OPTIONS.residual_tied then
-        if i ~= SEQUENCE_LENGTH then
-            -- Share weights on all but the last frame.
-            stub = reset_stub_to_conv4_3:sharedClone()
-        else
-            stub = stub_to_conv4_3:clone()
-        end
-    end
-    parallel_stubs_to_conv4_3:add(stub)
-end
-local conv4_3_merger = create_merger(
-    SEQUENCE_LENGTH, merge_type, args.weighted_merge)
+local previous_layer_index = 0
+local model_stubs = {}
+for i, layer_index in ipairs(MERGE_LAYER_INDICES) do
+    local stub = extract_stub(model, previous_layer_index + 1, layer_index)
+    local parallel_stubs = nn.ParallelTable()
+    local reset_stub = stub:clone()
+    reset_weights(reset_stub)
 
--- Conv4_3 -> Conv5_3
-local stub_conv4_3_to_conv5_3 = extract_stub(
-    model, CONV4_3_INDEX + 1, CONV5_3_INDEX)
-local parallel_stubs_conv4_3_to_conv5_3 = nn.ParallelTable()
--- For residual weights.
-local reset_stub_conv4_3_to_conv5_3 = stub_conv4_3_to_conv5_3:clone()
-reset_weights(reset_stub_conv4_3_to_conv5_3)
-for i = 1, (SEQUENCE_LENGTH/2) do
-    local stub
-    if weight_type == WEIGHT_TYPE_OPTIONS.untied then
-        stub = stub_conv4_3_to_conv5_3:clone()
-    elseif weight_type == WEIGHT_TYPE_OPTIONS.tied then
-        stub = stub_conv4_3_to_conv5_3:sharedClone()
-    elseif weight_type == WEIGHT_TYPE_OPTIONS.residual_tied then
-        if i ~= (SEQUENCE_LENGTH / 2) then
+    local input_length = SEQUENCE_LENGTH / (MERGE_INPUT_LENGTH^(i - 1))
+    for step = 1, input_length do
+        local new_stub
+        if weight_type == WEIGHT_TYPE_OPTIONS.untied then
+            new_stub = stub:clone()
+        elseif weight_type == WEIGHT_TYPE_OPTIONS.tied then
+            new_stub = stub:sharedClone()
+        elseif weight_type == WEIGHT_TYPE_OPTIONS.residual_tied then
             -- Share weights on all but the last frame.
-            stub = reset_stub_conv4_3_to_conv5_3:sharedClone()
-        else
-            stub = stub_conv4_3_to_conv5_3:clone()
+            if step ~= input_length then
+                new_stub = reset_stub:sharedClone()
+            else
+                new_stub = stub:clone()
+            end
         end
+        parallel_stubs:add(new_stub)
     end
-    parallel_stubs_conv4_3_to_conv5_3:add(stub)
+    local merger = create_merger(
+        input_length, MERGE_INPUT_LENGTH, merge_type, args.weighted_merge)
+    table.insert(model_stubs, parallel_stubs)
+    table.insert(model_stubs, merger)
+    previous_layer_index = layer_index
 end
-local conv5_3_merger = create_merger(SEQUENCE_LENGTH / 2, merge_type, args.weighted_merge)
 
--- Conv5_3 -> Output
-local stub_conv5_3_to_output = extract_stub(
-    model, CONV5_3_INDEX + 1, model:size())
+-- Last layer -> output
+local stub_to_output = extract_stub(
+    model, MERGE_LAYER_INDICES[#MERGE_LAYER_INDICES] + 1, model:size())
 
 local output_model = nn.Sequential()
 output_model:add(nn.SplitTable(1))
-output_model:add(parallel_stubs_to_conv4_3)
-output_model:add(conv4_3_merger)
-output_model:add(parallel_stubs_conv4_3_to_conv5_3)
-output_model:add(conv5_3_merger)
+for _, model_stub in ipairs(model_stubs) do
+    output_model:add(model_stub)
+end
 -- At this point we should have a table with exactly one element, which is the
 -- tensor of merged conv5 activations. Use JoinTable to convert it to a
 -- tensor. (We could also use SelectTable, but JoinTable will cause errors if
 -- the table somehow has more than one elements, which is nice.)
 output_model:add(nn.JoinTable(1))
-output_model:add(stub_conv5_3_to_output)
+output_model:add(stub_to_output)
 -- Output should be (1, batch_size, num_labels) to be compatible with
 -- DataParallelTable.
 output_model:add(nn.Unsqueeze(1))
-print(output_model)
 torch.save(args.output, output_model)
+print('Summing Conv5-3 and fc7!')
+print('Model saved to', output_model)
