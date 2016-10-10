81955850bd61e0f1991a65e258fc67f37dac7545
Achal Dave <achalddave@live.com>
2016-10-10 11:25:21 -0400

    log: Indentation/formatting

--- util/log.lua
+++ util/log.lua
@@ -13,67 +13,61 @@ log.usecolor = true
 log.outfile = nil
 log.level = "trace"
 
-
 local modes = {
-  { name = "trace", color = "\27[34m", },
-  { name = "debug", color = "\27[36m", },
-  { name = "info",  color = "\27[32m", },
-  { name = "warn",  color = "\27[33m", },
-  { name = "error", color = "\27[31m", },
-  { name = "fatal", color = "\27[35m", },
+    { name = "trace", color = "\27[34m", },
+    { name = "debug", color = "\27[36m", },
+    { name = "info",  color = "\27[32m", },
+    { name = "warn",  color = "\27[33m", },
+    { name = "error", color = "\27[31m", },
+    { name = "fatal", color = "\27[35m", },
 }
-
 
 local levels = {}
 for i, v in ipairs(modes) do
-  levels[v.name] = i
+    levels[v.name] = i
 end
 
 local _tostring = tostring
 
 local tostring = function(...)
-  local t = {}
-  for i = 1, select('#', ...) do
-    local x = select(i, ...)
-    t[#t + 1] = _tostring(x)
-  end
-  return table.concat(t, " ")
+    local t = {}
+    for i = 1, select('#', ...) do
+        local x = select(i, ...)
+        t[#t + 1] = _tostring(x)
+    end
+    return table.concat(t, " ")
 end
-
 
 for i, x in ipairs(modes) do
-  local nameupper = x.name:upper()
-  log[x.name] = function(...)
-    
-    -- Return early if we're below the log level
-    if i < levels[log.level] then
-      return
+    local nameupper = x.name:upper()
+    log[x.name] = function(...)
+        -- Return early if we're below the log level
+        if i < levels[log.level] then
+            return
+        end
+
+        local msg = tostring(...)
+        local info = debug.getinfo(2, "Sl")
+        local lineinfo = info.short_src .. ":" .. info.currentline
+
+        -- Output to console
+        print(string.format("%s[%-6s%s]%s %s: %s",
+                            log.usecolor and x.color or "",
+                            nameupper,
+                            os.date("%H:%M:%S"),
+                            log.usecolor and "\27[0m" or "",
+                            lineinfo,
+                            msg))
+
+        -- Output to log file
+        if log.outfile then
+        local fp = io.open(log.outfile, "a")
+        local str = string.format("[%-6s%s] %s: %s\n",
+                                  nameupper, os.date(), lineinfo, msg)
+        fp:write(str)
+        fp:close()
+        end
     end
-
-    local msg = tostring(...)
-    local info = debug.getinfo(2, "Sl")
-    local lineinfo = info.short_src .. ":" .. info.currentline
-
-    -- Output to console
-    print(string.format("%s[%-6s%s]%s %s: %s",
-                        log.usecolor and x.color or "",
-                        nameupper,
-                        os.date("%H:%M:%S"),
-                        log.usecolor and "\27[0m" or "",
-                        lineinfo,
-                        msg))
-
-    -- Output to log file
-    if log.outfile then
-      local fp = io.open(log.outfile, "a")
-      local str = string.format("[%-6s%s] %s: %s\n",
-                                nameupper, os.date(), lineinfo, msg)
-      fp:write(str)
-      fp:close()
-    end
-
-  end
 end
-
 
 return log
