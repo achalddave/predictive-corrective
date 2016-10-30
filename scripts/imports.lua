670ff427ddff3f2eb2ec7345580165201d3d5ff0
Achal Dave <achalddave@live.com>
2016-10-30 16:19:14 -0400

    Add comment to scripts/imports.lua

--- scripts/imports.lua
+++ scripts/imports.lua
@@ -1,3 +1,11 @@
+--[[ Useful for starting an interpreter with all the necessary imports:
+--
+-- Usage:
+-- ~ th -i scripts/imports.lua
+--
+-- NOTE: Do not 'require' this file except from an interactive session. If you
+-- need these imports, copy and paste them so they are explicitly imported.
+--]]
 local argparse = require 'argparse'
 local cudnn = require 'cudnn'
 local cutorch = require 'cutorch'
@@ -6,4 +14,3 @@ local nn = require 'nn'
 require 'rnn'
 require 'layers/init'
 require 'cunn'
-
