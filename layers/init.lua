d0aac76340cdcfb66be659e7a38da622f7dcc2fa
Achal Dave <achalddave@live.com>
2016-12-20 12:14:51 -0500

    Remove reference to deleted CAvgTable

--- layers/init.lua
+++ layers/init.lua
@@ -1,6 +1,5 @@
 require 'layers/ConcatTableFunctional'
 require 'layers/ConcatTableFunctionalReinit'
-require 'layers/CAvgTable'
 require 'layers/CRollingDiffTable'
 require 'layers/CCumSumTable'
 require 'layers/PeriodicResidualTable'
