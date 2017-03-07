49dc7db5355a9ad085f5e8aa6b53c7a59b7d054c
Achal Dave <achalddave@live.com>
2017-03-07 14:30:59 -0500

    Add reinitialize rate for PredictiveCorrectiveRecurrent

--- layers/PredictiveCorrectiveRecurrent.lua
+++ layers/PredictiveCorrectiveRecurrent.lua
@@ -1,18 +1,34 @@
+--[[
+-- Module for a recurrent Predictive-Corrective block.
+--
+-- The main export of this module is nn.PredictiveCorrectiveRecurrent
+--
+-- Defines the following helper modules:
+--     - nn.InputCouplerRecurrent: Used for nn.CRollingDiffRecurrent
+--     - nn.CRollingDiffRecurrent
+--     - nn.CCumSumRecurrent
+--]]
 local nn = require 'nn'
 local rnn = require 'rnn'
 local torch = require 'torch'
+local __ = require 'moses'
 
 local InitUpdateRecurrent = require 'layers/InitUpdateRecurrent'
 
 local InputCouplerRecurrent, parent = torch.class(
     'nn.InputCouplerRecurrent', 'nn.Recurrence')
 
-function InputCouplerRecurrent:__init(rho)
+function InputCouplerRecurrent:__init(reinitialize_rate, rho)
     --[[
     -- Output rolling couples of inputs:
     --     output(t) = {input(t), input(t-1)}
+    -- For the first time step, and on every reinitialization (which happens at
+    -- the reinitialize_rate), we output
+    --     output(t) = {input(t), zeroTensor}
     --
-    -- Therefore, the hidden state at time t is {input(t-1), input(t-2)}.
+    -- This is used to implement the differencer.
+    --
+    -- The hidden state at time t is {input(t-1), input(t-2)}.
     --
     -- The main reason this is a separate class is so we can override
     -- getHiddenState so that the output size does not need to be specified, as
@@ -21,6 +37,7 @@ function InputCouplerRecurrent:__init(rho)
     --]]
     nn.AbstractRecurrent.__init(self, rho or 9999)
 
+    self.reinitialize_rate = reinitialize_rate or math.huge
     -- Given input {a, {b, c}}, outputs {a, b}.
     -- In our case, a, b, c are the inputs at time t, t-1, and t-2 respectively.
     local pair_selector = nn.ConcatTable()
@@ -37,37 +54,93 @@ function InputCouplerRecurrent:__init(rho)
     self.typeTensor = torch.Tensor()
 end
 
+function InputCouplerRecurrent:_is_reinit(step)
+    -- Apparently (0 % math.huge) is not == 0, so we explicitly check for the
+    -- first step.
+    return step == 1 or (step - 1) % self.reinitialize_rate == 0
+end
+
 function InputCouplerRecurrent:getHiddenState(step, input)
     local prevOutput
-    if step == 0 then
+    if self:_is_reinit(step+1) then
         if input then
             self.zeroTensor = self:recursiveResizeZero(
                 self.zeroTensor, {input:size(), input:size()})
         end
-        prevOutput = self.userPrevOutput or self.outputs[step] or self.zeroTensor
+        prevOutput = self.userPrevOutput or self.zeroTensor
     else
         -- previous output of this module
         prevOutput = self.outputs[step]
     end
-    -- call getHiddenState on recurrentModule as they may contain
-    -- AbstractRecurrent instances...
     return {prevOutput, nn.Container.getHiddenState(self, step)}
+end
+
+function InputCouplerRecurrent:_updateGradInput(input, gradOutput)
+    --[[
+    -- Args:
+    --     input
+    --     gradOutput: Table containing two elements of same size as input.
+    --]]
+    assert(self.step > 1, "expecting at least one updateOutput")
+    local step = self.updateGradInputStep - 1
+    assert(step >= 1)
+
+    -- set the output/gradOutput states of current Module
+    local recurrentModule = self:getStepModule(step)
+
+    -- The output of this step was fed as input to the next step; get the
+    -- gradient from the next step and add it to gradOutput (which is the
+    -- gradient from the current step).
+    local _gradOutput = self:getGradHiddenState(step)[1]
+    self._gradOutputs[step] = nn.rnn.recursiveCopy(self._gradOutputs[step], _gradOutput)
+    nn.rnn.recursiveAdd(self._gradOutputs[step], gradOutput)
+    gradOutput = self._gradOutputs[step]
+
+    local gradInputTable = recurrentModule:updateGradInput(
+        {input, self:getHiddenState(step-1)[1]}, gradOutput)
+
+    -- Set the gradient for the previous step (whose output was provided as
+    -- input to the current step). If our current step is a re-initialization
+    -- step, then zero out the gradient for the previous step (since it was not
+    -- used as input to the current step).
+    local previous_step_grad = __.slice(gradInputTable, 2, #gradInputTable)
+    if self:_is_reinit(step) then
+        if not self.zeroGradTensor then
+            self.zeroGradTensor = nn.rnn.recursiveResizeAs(
+                self.zeroGradTensor, previous_step_grad)
+            nn.rnn.recursiveFill(self.zeroGradTensor, 0)
+        end
+        self:setGradHiddenState(step - 1, self.zeroGradTensor)
+    else
+        self:setGradHiddenState(step - 1, previous_step_grad)
+    end
+
+    return gradInputTable[1]
 end
 
 local CRollingDiffRecurrent, parent = torch.class(
     'nn.CRollingDiffRecurrent', 'nn.Sequential')
-function CRollingDiffRecurrent:__init(rho)
+function CRollingDiffRecurrent:__init(reinitialize_rate, rho)
     --[[
     -- Computes difference between consecutive inputs:
     --     output(t) = input(t) - input(t-1)
+    --
+    -- Use InputCouplerRecurrent, and produce successive differences between
+    -- pairs.  InputCouplerRecurrent outputs {input(t), input(t-1)}, which is
+    -- then passed through a CSubTable() in this module.  Note that the
+    -- re-initialization is naturally handled by InputCouplerRecurrent, which
+    -- will ensure that input(t-1) is a zero tensor upon re-initialization.
     --]]
     parent.__init(self)
-    self.modules = {nn.InputCouplerRecurrent(rho), nn.CSubTable()}
+    self.modules = {
+        nn.InputCouplerRecurrent(reinitialize_rate, rho),
+        nn.CSubTable()
+    }
 end
 
 local CCumSumRecurrent, parent = torch.class(
     'nn.CCumSumRecurrent', 'nn.Recurrence')
-function CCumSumRecurrent:__init(rho)
+function CCumSumRecurrent:__init(reinitialize_rate, rho)
     --[[
     -- Output componentwise cumulative sum of inputs:
     --     output(t) = input(t) + output(t-1)
@@ -78,6 +151,8 @@ function CCumSumRecurrent:__init(rho)
     --]]
     nn.AbstractRecurrent.__init(self, rho or 9999)
 
+    self.reinitialize_rate = reinitialize_rate or math.huge
+
     self.recurrentModule = nn.CAddTable()
 
     self.module = self.recurrentModule
@@ -87,32 +162,41 @@ function CCumSumRecurrent:__init(rho)
     self.typeTensor = torch.Tensor()
 end
 
+function CCumSumRecurrent:_is_reinit(step)
+    -- Apparently (0 % math.huge) is not == 0, so we explicitly check for the
+    -- first step.
+    return step == 1 or (step - 1) % self.reinitialize_rate == 0
+end
+
 function CCumSumRecurrent:getHiddenState(step, input)
     local prevOutput
-    if step == 0 then
+    if self:_is_reinit(step+1) then
         if input then
             self.zeroTensor = self:recursiveResizeZero(
                 self.zeroTensor, input:size())
         end
-        prevOutput = self.userPrevOutput or self.outputs[step] or self.zeroTensor
+        prevOutput = self.userPrevOutput or self.zeroTensor
     else
         -- previous output of this module
         prevOutput = self.outputs[step]
     end
-    -- call getHiddenState on recurrentModule as they may contain
-    -- AbstractRecurrent instances...
     return {prevOutput, nn.Container.getHiddenState(self, step)}
+end
+
+function CCumSumRecurrent:_updateGradInput(input, gradOutput)
+    return InputCouplerRecurrent._updateGradInput(self, input, gradOutput)
 end
 
 local PredictiveCorrectiveRecurrent, parent = torch.class(
     'nn.PredictiveCorrectiveRecurrent', 'nn.Sequential')
 
-function PredictiveCorrectiveRecurrent:__init(init, update, rho)
+function PredictiveCorrectiveRecurrent:__init(
+        init, update, reinitialize_rate, rho)
     parent.__init(self)
 
     self.modules = {
-        nn.CRollingDiffRecurrent(rho),
-        nn.InitUpdateRecurrent(init, update, rho),
+        nn.CRollingDiffRecurrent(reinitialize_rate, rho),
+        nn.InitUpdateRecurrent(init, update, reinitialize_rate, rho),
         nn.CCumSumRecurrent(rho)
     }
 end
--- tests/PredictiveCorrectiveRecurrent_test.lua
+++ tests/PredictiveCorrectiveRecurrent_test.lua
@@ -1,13 +1,14 @@
 local nn = require 'nn'
 local torch = require 'torch'
+local __ = require 'moses'
 
 local test_util = require 'tests/test_util'
-require 'layers/PredictiveCorrectiveRecurrent'
+require 'layers/init'
 
 local function test_no_reinit()
     local init = nn.MulConstant(1)
     local update = nn.MulConstant(2)
-    local model = nn.PredictiveCorrectiveRecurrent(init, update, math.huge)
+    local model = nn.PredictiveCorrectiveRecurrent(init, update)
     model = nn.Sequencer(model)
 
     local inputs = {}
@@ -19,6 +20,151 @@ local function test_no_reinit()
     assert(test_util.equals(outputs[2], 2*(inputs[2] - inputs[1]) + outputs[1]))
     assert(test_util.equals(outputs[3], 2*(inputs[3] - inputs[2]) + outputs[2]))
     assert(test_util.equals(outputs[4], 2*(inputs[4] - inputs[3]) + outputs[3]))
+end
+
+local function test_no_reinit_backward()
+    local init = nn.MulConstant(1)
+    local update = nn.MulConstant(2)
+    local model = nn.PredictiveCorrectiveRecurrent(init, update, math.huge)
+    model = nn.Sequencer(model)
+
+    local inputs = {}
+    for i = 1, 4 do inputs[i] = torch.rand(5, 5) end
+
+    local outputs = model:forward(inputs)
+    -- Just test that this does not error.
+    model:backward(inputs, inputs)
+end
+
+local function test_backward_one_step()
+    local init = nn.MulConstant(1)
+    local update = nn.MulConstant(2)
+    local reinit = 4
+    local rho = 4
+
+    local model = nn.PredictiveCorrectiveRecurrent(init, update, reinit, rho)
+    model = nn.Sequencer(model)
+
+    local model_block = nn.Sequential()
+        :add(nn.CRollingDiffTable(reinit))
+        :add(nn.PeriodicResidualTable(reinit, init:clone(), update:clone()))
+        :add(nn.CCumSumTable(reinit))
+
+    local inputs = {torch.rand(5, 5)}
+
+    local outputs_block = model_block:forward(inputs)
+    local outputs = model:forward(inputs)
+
+    assert(test_util.equals(outputs_block[1], outputs[1]))
+
+    local gradients = outputs
+
+    local computed_gradients = model:backward(inputs, gradients)
+    local computed_gradients_block = model_block:backward(inputs, gradients)
+    assert(test_util.equals(computed_gradients[1], computed_gradients_block[1]))
+end
+
+local function test_backward_two_steps()
+    local init = nn.MulConstant(1)
+    local update = nn.MulConstant(2)
+    local reinit = 2
+    local rho = 2
+
+    local function clone(table_of_tensors)
+        return __.invoke(table_of_tensors, function(x) return x:clone() end)
+    end
+
+    -- We don't want to put the assertions in a loop so that a failed assertion
+    -- points to an explicit step. To do this, we create two separate methods
+    -- for 4 vs. 8 inputs.
+    local function compare_modules_4(module1, module2)
+        local inputs = {}
+        for i = 1, 4 do inputs[i] = torch.rand(5, 5) end
+        local outputs1 = clone(module1:forward(inputs))
+        local outputs2 = clone(module2:forward(inputs))
+
+        assert(test_util.equals(outputs1[1], outputs2[1]))
+        assert(test_util.equals(outputs1[2], outputs2[2]))
+        assert(test_util.equals(outputs1[3], outputs2[3]))
+        assert(test_util.equals(outputs1[4], outputs2[4]))
+
+        local gradients_out = clone(outputs1)
+        local gradients1 = module1:backward(inputs, gradients_out)
+        local gradients2 = module2:backward(inputs, gradients_out)
+
+        assert(test_util.equals(gradients1[1], gradients2[1]))
+        assert(test_util.equals(gradients1[2], gradients2[2]))
+        assert(test_util.equals(gradients1[3], gradients2[3]))
+        assert(test_util.equals(gradients1[4], gradients2[4]))
+    end
+
+    local function compare_modules_8(module1, module2)
+        local inputs = {}
+        for i = 1, 8 do inputs[i] = torch.rand(5, 5) end
+        local outputs1 = clone(module1:forward(inputs))
+        local outputs2 = clone(module2:forward(inputs))
+
+        assert(test_util.equals(outputs1[1], outputs2[1]))
+        assert(test_util.equals(outputs1[2], outputs2[2]))
+        assert(test_util.equals(outputs1[3], outputs2[3]))
+        assert(test_util.equals(outputs1[4], outputs2[4]))
+        assert(test_util.equals(outputs1[5], outputs2[5]))
+        assert(test_util.equals(outputs1[6], outputs2[6]))
+        assert(test_util.equals(outputs1[7], outputs2[7]))
+        assert(test_util.equals(outputs1[8], outputs2[8]))
+
+        local gradients_out = clone(outputs1)
+        local gradients1 = module1:backward(inputs, gradients_out)
+        local gradients2 = module2:backward(inputs, gradients_out)
+
+        assert(test_util.equals(gradients1[1], gradients2[1]))
+        assert(test_util.equals(gradients1[2], gradients2[2]))
+        assert(test_util.equals(gradients1[3], gradients2[3]))
+        assert(test_util.equals(gradients1[4], gradients2[4]))
+        assert(test_util.equals(gradients1[5], gradients2[5]))
+        assert(test_util.equals(gradients1[6], gradients2[6]))
+        assert(test_util.equals(gradients1[7], gradients2[7]))
+        assert(test_util.equals(gradients1[8], gradients2[8]))
+    end
+
+
+    -- Test CRollingDiffRecurrent
+    do
+        local differ = nn.Sequencer(nn.CRollingDiffRecurrent(reinit))
+        local differ_block = nn.CRollingDiffTable(reinit)
+        compare_modules_4(differ, differ_block)
+        compare_modules_8(differ, differ_block)
+    end
+
+    -- Test InitUpdateRecurrent
+    do
+        local init_update = nn.Sequencer(nn.InitUpdateRecurrent(
+            init:clone(), update:clone(), reinit))
+        local init_update_block = nn.PeriodicResidualTable(
+            reinit, init:clone(), update:clone())
+        compare_modules_4(init_update, init_update_block)
+        compare_modules_8(init_update, init_update_block)
+    end
+
+    -- Test CCumSumRecurrent
+    do
+        local summer = nn.Sequencer(nn.CCumSumRecurrent(reinit))
+        local summer_block = nn.CCumSumTable(reinit)
+        compare_modules_4(summer, summer_block)
+        compare_modules_8(summer, summer_block)
+    end
+
+    -- Test PredictiveCorrectiveRecurrent
+    do
+        local model = nn.Sequencer(
+            nn.PredictiveCorrectiveRecurrent(init, update, reinit, rho))
+        local model_block = nn.Sequential()
+            :add(nn.CRollingDiffTable(reinit))
+            :add(nn.PeriodicResidualTable(reinit, init:clone(), update:clone()))
+            :add(nn.CCumSumTable(reinit))
+        compare_modules_4(model, model_block)
+        compare_modules_8(model, model_block)
+    end
 end
 
 local function test_reinit_every_time()
@@ -66,5 +212,8 @@ local function test_clearState()
 end
 
 test_util.run_test(test_no_reinit, 'No reinit test')
+test_util.run_test(test_backward_one_step, 'Test backward one step')
+test_util.run_test(test_backward_two_steps, 'Test backward two steps')
+test_util.run_test(test_no_reinit_backward, 'Backward')
 test_util.run_test(test_reinit_every_time, 'Reinit every time')
 test_util.run_test(test_clearState, 'clearState')
