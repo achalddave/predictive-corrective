7053522709e9273cd384647682cefec24128c5e0
Achal Dave <achalddave@live.com>
2017-03-07 14:29:18 -0500

    InitUpdateRecurrent: Add reinitialize rate (+tests)

--- layers/InitUpdateRecurrent.lua
+++ layers/InitUpdateRecurrent.lua
@@ -9,7 +9,7 @@ require 'util/strict'
 local InitUpdateRecurrent, parent = torch.class(
     'nn.InitUpdateRecurrent', 'nn.Recurrent')
 
-function InitUpdateRecurrent:__init(init, update, rho)
+function InitUpdateRecurrent:__init(init, update, reinitialize_rate, rho)
     --[[
     -- Similar to nn.Recursor, but with a separate 'init' module.
     --
@@ -20,34 +20,62 @@ function InitUpdateRecurrent:__init(init, update, rho)
     --
     -- This class is based off of nn.Recursor from the rnn package from
     -- element-research.
+    --
+    -- Args:
+    --    init (nn.Module): Initialization module
+    --    update (nn.Module): Update module
+    --    reinitialize_rate (int): If specified, re-initialize the module at
+    --        this rate (i.e. run the init module on every reinitialize_rate'th
+    --        input).
+    --    rho (int): See nn.Recurrence doc.
     --]]
 
     nn.AbstractRecurrent.__init(self, rho)
 
-    self.initialModule = init
-    self.update = update
+    self.reinitialize_rate = reinitialize_rate ~= nil and reinitialize_rate
+                                                      or math.huge
 
-    -- Recurrent module will be passed {input_t, output_{t-1}} by the parent
-    -- class; we select the first input (input_t) and pass it through
-    -- self.update.
-    self.recurrentModule = nn.Sequential()
-        :add(nn.SelectTable(1))
-        :add(self.update)
-    self.sharedClones[2] = self.recurrentModule
+    self.initialModule = init
+    self.recurrentModule = update
+    self.sharedClones = {self.initialModule, self.recurrentModule}
 
     self.modules = {self.initialModule, self.recurrentModule}
 end
 
+function InitUpdateRecurrent:_is_reinit(step)
+    -- Apparently (0 % math.huge) is not == 0, so we explicitly check for the
+    -- first step.
+    return step == 1 or (step - 1) % self.reinitialize_rate == 0
+end
+
+function InitUpdateRecurrent:_last_reinit(i)
+    -- When did we last reinitialize before the ith input?
+    return (math.floor((i - 1) / self.reinitialize_rate)
+                * self.reinitialize_rate + 1)
+end
+
+function InitUpdateRecurrent:maskZero()
+    error("InitUpdateRecurrent doesn't support maskZero as it uses a " ..
+          "different module for the first time-step. Use nn.Recurrence " ..
+          "instead.")
+end
+
+function InitUpdateRecurrent:trimZero()
+    error("InitUpdateRecurrent doesn't support trimZero as it uses a " ..
+          "different module for the first time-step. Use nn.Recurrence " ..
+          "instead.")
+end
+
 function InitUpdateRecurrent:getStepModule(step)
     assert(step, "expecting step at arg 1")
-    assert(step > 1, 'step must be >1 since first step is for initialModule.')
-    local recurrentModule = self.sharedClones[step]
-    if not recurrentModule then
-        recurrentModule = self.recurrentModule:stepClone()
-        self.sharedClones[step] = recurrentModule
+    local module = self.sharedClones[step]
+    if not module then
+        module = self:_is_reinit(step) and self.initialModule:stepClone()
+                                       or self.recurrentModule:stepClone()
+        self.sharedClones[step] = module
         self.nSharedClone = __.size(self.sharedClones)
     end
-    return recurrentModule
+    return module
 end
 
 function InitUpdateRecurrent:_updateGradInput(input, gradOutput)
@@ -57,46 +85,41 @@ function InitUpdateRecurrent:_updateGradInput(input, gradOutput)
     nn.Recurrent._updateGradInput(self, input, gradOutput)
 end
 
--- Updated since our sharedClones start at index 2.
-function InitUpdateRecurrent:recycle()
-   self.nSharedClone = self.nSharedClone or __.size(self.sharedClones)
-
-   local rho = math.max(self.rho + 1, self.nSharedClone)
-   if self.sharedClones[self.step] == nil then
-      self.sharedClones[self.step] = self.sharedClones[self.step-rho]
-      self.sharedClones[self.step-rho] = nil
-      self._gradOutputs[self.step] = self._gradOutputs[self.step-rho]
-      self._gradOutputs[self.step-rho] = nil
-   end
-
-   self.outputs[self.step-rho-1] = nil
-   self.gradInputs[self.step-rho-1] = nil
-
-   return self
-end
-
--- Updated since our sharedClones start at index 2.
+-- We need to override :forget() so that we compact sharedClones and gradOutputs
+-- in a way that keeps the initialization modules in reinit steps, and the
+-- recurrent modules in update steps.
 function InitUpdateRecurrent:forget()
-    local offset = 1
-
-    -- the recurrentModule may contain an AbstractRecurrent instance (issue 107)
+    -- The {initial,recurrent}Modules may contain an AbstractRecurrent instance
+    -- (issue 107).
     nn.Container.forget(self)
 
-    -- bring all states back to the start of the sequence buffers
+    -- Bring all states back to the start of the sequence buffers.
     if self.train ~= false then
         self.outputs = {}
         self.gradInputs = {}
-        local compactSharedClones = __.compact(self.sharedClones)
-        self.sharedClones = {}
-        for i, v in ipairs(compactSharedClones) do
-            self.sharedClones[i + offset] = v
+        local next_init = 1
+        local next_update = 2
+        local sharedClones = {}
+        local gradOutputs = {}
+        -- Compact sharedClones and gradOutput, ensuring that we don't put a
+        -- recurrentModule in an init step, or an initModule in an update step.
+        for _, step in ipairs(__.sort(__.keys(self.sharedClones))) do
+            local new_step
+            if self:_is_reinit(step) then
+                new_step = next_init
+                next_init = next_init + self.reinitialize_rate
+            else
+                new_step = next_update
+                next_update = next_update + 1
+                if self:_is_reinit(next_update) then
+                    next_update = next_update + 1
+                end
+            end
+            sharedClones[new_step] = self.sharedClones[step]
+            gradOutputs[new_step] = self._gradOutputs[step]
         end
-
-        local compactGradOutputs = __.compact(self._gradOutputs)
-        self._gradOutputs = {}
-        for i, v in ipairs(compactGradOutputs) do
-            self._gradOutputs[i + offset] = v
-        end
+        self.sharedClones = sharedClones
+        self._gradOutputs = gradOutputs
         self.gradOutputs = {}
         if self.cells then
             self.cells = {}
@@ -104,32 +127,99 @@ function InitUpdateRecurrent:forget()
         end
     end
 
-    -- forget the past inputs; restart from first step
+    -- Forget the past inputs; restart from first step.
     self.step = 1
 
-    if not self.rmInSharedClones then
-        -- Asserts that issue 129 is solved. In forget as it is often called.
-        -- Asserts that self.recurrentModule is part of the sharedClones.
-        -- Since its used for evaluation, it should be used for training.
-        local nClone, maxIdx = 0, offset
-        for k,v in pairs(self.sharedClones) do -- to prevent odd bugs
-            if torch.pointer(v) == torch.pointer(self.recurrentModule) then
-                self.rmInSharedClones = true
-                maxIdx = math.max(k, maxIdx)
-            end
-            nClone = nClone + 1
+    -- AbstractRecurrent:forget() explicitly checks if self.recurrentModule and
+    -- self.initialModule are in self.sharedClones, and adds them if they are
+    -- not. I don't know why this is necessary, so I'm not implementing it in
+    -- case I introduce a bug. This assert will ensure the modules are in
+    -- sharedClones, and if it fails, I can can debug to figure out why.
+    assert(__.any(self.sharedClones, function(clone)
+        return torch.pointer(clone) == torch.pointer(self.recurrentModule)
+    end), 'self.recurrentModule not in self.sharedClones')
+
+    assert(__.any(self.sharedClones, function(clone)
+        return torch.pointer(clone) == torch.pointer(self.initialModule)
+    end), 'self.initialModule not in self.sharedClones')
+
+    return self
+end
+
+function InitUpdateRecurrent:recycle()
+    self.nSharedClone = self.nSharedClone or __.size(self.sharedClones)
+
+    local rho = self.rho + 1
+    if self.sharedClones[self.step] == nil then
+        local previous_step
+        if self:_is_reinit(self.step) == self:_is_reinit(self.step - rho) then
+            previous_step = self.step - rho
+        elseif self:_is_reinit(self.step) then
+            previous_step = self:_last_reinit(self.step - rho)
         end
-        if nClone > 1 then
-            if not self.rmInSharedClones then
-                print"WARNING : recurrentModule should be added to sharedClones in constructor."
-                print"Adding it for you."
-                assert(torch.type(self.sharedClones[maxIdx]) == torch.type(self.recurrentModule))
-                self.recurrentModule = self.sharedClones[maxIdx]
-                self.rmInSharedClones = true
-            end
+        if previous_step ~= nil then
+            self.sharedClones[self.step] = self.sharedClones[previous_step]
+            self.sharedClones[previous_step] = nil
+            self._gradOutputs[self.step] = self._gradOutputs[previous_step]
+            self._gradOutputs[previous_step] = nil
         end
     end
+
+    self.outputs[self.step-rho-1] = nil
+    self.gradInputs[self.step-rho-1] = nil
+
     return self
+end
+
+
+function InitUpdateRecurrent:updateOutput(input)
+    -- output(t) = transfer(feedback(output_(t-1)) + input(input_(t)))
+    local module
+    if self.train ~= false then
+        -- set/save the output states
+        self:recycle()
+        module = self:getStepModule(self.step)
+    else
+        -- self.output is the previous output of this module
+        module = self:_is_reinit(self.step) and self.initialModule
+                                            or self.recurrentModule
+    end
+    local output = module:updateOutput(input)
+
+    self.outputs[self.step] = output
+    self.output = output
+    self.step = self.step + 1
+    self.gradPrevOutput = nil
+    self.updateGradInputStep = nil
+    self.accGradParametersStep = nil
+    return self.output
+end
+
+function InitUpdateRecurrent:_updateGradInput(input, gradOutput)
+    assert(self.step > 1, "expecting at least one updateOutput")
+    local step = self.updateGradInputStep - 1
+    local module = self:getStepModule(step)
+    return module:updateGradInput(input, gradOutput)
+end
+
+function InitUpdateRecurrent:_accGradParameters(input, gradOutput, scale)
+    local step = self.accGradParametersStep - 1
+
+    local gradOutput = (step == self.step-1) and gradOutput
+                                             or self._gradOutputs[step]
+
+    local module = self:getStepModule(step)
+    module:accGradParameters(input, gradOutput, scale)
+end
+
+function InitUpdateRecurrent:includingSharedClones(f)
+    nn.AbstractRecurrent.includingSharedClones(self, f)
+end
+
+function InitUpdateRecurrent:reinforce(reward)
+    -- I don't know what this method is supposed to do, so I'm not going to try
+    -- to implemenet it.
+    error('Reinforce not implemented for InitUpdateRecurrent.')
 end
 
 function InitUpdateRecurrent:__tostring__()
@@ -149,7 +239,7 @@ function InitUpdateRecurrent:__tostring__()
 
     str = str .. line .. tab .. next ..
           '(t>1): ' ..
-          tostring(self.update):gsub(line, line .. tab .. ext)
+          tostring(self.recurrentModule):gsub(line, line .. tab .. ext)
 
     str = str .. line .. tab .. last .. 'output'
     str = str .. line .. '}'
--- tests/InitUpdateRecurrent_test.lua
+++ tests/InitUpdateRecurrent_test.lua
@@ -8,7 +8,8 @@ require 'layers/InitUpdateRecurrent'
 local function test_init_only()
     local init = nn.MulConstant(1)
     local update = nn.MulConstant(2)
-    local model = nn.InitUpdateRecurrent(init, update, 1)
+    local model = nn.InitUpdateRecurrent(
+        init, update, math.huge --[[reinitialize_rate]], 1 --[[rho]])
 
     local a = {}
     for i = 1, 4 do a[i] = torch.rand(5, 5) end
@@ -27,7 +28,8 @@ end
 local function test_updates()
     local init = nn.MulConstant(1)
     local update = nn.MulConstant(2)
-    local model = nn.InitUpdateRecurrent(init, update, 1)
+    local model = nn.InitUpdateRecurrent(
+        init, update, math.huge --[[reinitialize_rate]], 1 --[[rho]])
 
     local a = {}
     for i = 1, 6 do a[i] = torch.rand(5, 5) end
@@ -49,11 +51,73 @@ local function test_updates()
     assert(test_util.equals(b[6], 2*a[6]))
 end
 
+local function test_reinit()
+    local init = nn.MulConstant(1)
+    local update = nn.MulConstant(2)
+    local model = nn.InitUpdateRecurrent(
+        init, update, 3 --[[reinitialize_rate]], 1 --[[rho]])
+
+    local a = {}
+    for i = 1, 6 do a[i] = torch.rand(5, 5) end
+
+    local b = {}
+    b[1] = model:forward(a[1]):clone()
+    b[2] = model:forward(a[2]):clone()
+    b[3] = model:forward(a[3]):clone()
+    b[4] = model:forward(a[4]):clone()
+    b[5] = model:forward(a[5]):clone()
+    b[6] = model:forward(a[6]):clone()
+
+    assert(test_util.equals(b[1], a[1]))
+    assert(test_util.equals(b[2], 2*a[2]))
+    assert(test_util.equals(b[3], 2*a[3]))
+    assert(test_util.equals(b[4], a[4]))
+    assert(test_util.equals(b[5], 2*a[5]))
+    assert(test_util.equals(b[6], 2*a[6]))
+end
+
 local function test_check_sharedClones_table_sanity()
     local init = nn.MulConstant(1)
     local update = nn.MulConstant(2)
-    local model = nn.InitUpdateRecurrent(init, update, 2)
-    assert(model.sharedClones[1] == nil)
+    local model = nn.InitUpdateRecurrent(
+        init, update, math.huge--[[reinitialize_rate]], 2 --[[rho]])
+
+    model:forward(torch.rand(5, 5))
+    model:forward(torch.rand(5, 5))
+    model:forward(torch.rand(5, 5))
+    model:forward(torch.rand(5, 5))
+    assert(__.size(model.sharedClones) == 4)
+    assert(model.sharedClones[1] == model.initialModule)
+    assert(model.sharedClones[2] == model.recurrentModule)
+    assert(model.sharedClones[3].constant_scalar == 2)
+    assert(model.sharedClones[4].constant_scalar == 2)
+
+    model:forward(torch.rand(5, 5))
+    model:forward(torch.rand(5, 5))
+    model:forward(torch.rand(5, 5))
+    assert(model.sharedClones[1] == model.initialModule)
+    assert(model.sharedClones[2] == nil)
+    assert(model.sharedClones[3] == nil)
+    assert(model.sharedClones[4] == nil)
+    assert(model.sharedClones[5].constant_scalar == 2)
+    assert(model.sharedClones[6].constant_scalar == 2)
+    assert(model.sharedClones[7].constant_scalar == 2)
+    assert(__.size(model.sharedClones) == 4)
+
+    model:forget()
+    assert(model.sharedClones[1] == model.initialModule)
+    assert(model.sharedClones[2].constant_scalar == 2)
+    assert(model.sharedClones[3].constant_scalar == 2)
+    assert(model.sharedClones[4].constant_scalar == 2)
+    assert(model.sharedClones[5] == nil)
+    assert(__.size(model.sharedClones) == 4)
+end
+
+local function test_check_sharedClones_table_sanity_reinit()
+    local init = nn.MulConstant(1)
+    local update = nn.MulConstant(2)
+    local model = nn.InitUpdateRecurrent(
+        init, update, 3 --[[reinitialize_rate]], 2 --[[rho]])
 
     model:forward(torch.rand(5, 5))
     model:forward(torch.rand(5, 5))
@@ -61,9 +125,9 @@ local function test_check_sharedClones_table_sanity()
     model:forward(torch.rand(5, 5))
     assert(__.size(model.sharedClones) == 3)
     assert(model.sharedClones[1] == nil)
-    assert(model.sharedClones[2] ~= nil)
-    assert(model.sharedClones[3] ~= nil)
-    assert(model.sharedClones[4] ~= nil)
+    assert(model.sharedClones[2].constant_scalar == 2)
+    assert(model.sharedClones[3].constant_scalar == 2)
+    assert(model.sharedClones[4].constant_scalar == 1)
 
     model:forward(torch.rand(5, 5))
     model:forward(torch.rand(5, 5))
@@ -72,24 +136,26 @@ local function test_check_sharedClones_table_sanity()
     assert(model.sharedClones[2] == nil)
     assert(model.sharedClones[3] == nil)
     assert(model.sharedClones[4] == nil)
-    assert(model.sharedClones[5] ~= nil)
-    assert(model.sharedClones[6] ~= nil)
-    assert(model.sharedClones[7] ~= nil)
+    assert(model.sharedClones[5].constant_scalar == 2)
+    assert(model.sharedClones[6].constant_scalar == 2)
+    assert(model.sharedClones[7].constant_scalar == 1)
     assert(__.size(model.sharedClones) == 3)
 
     model:forget()
-    assert(model.sharedClones[1] == nil)
-    assert(model.sharedClones[2] ~= nil)
-    assert(model.sharedClones[3] ~= nil)
-    assert(model.sharedClones[4] ~= nil)
+    assert(model.sharedClones[1] == model.initialModule)
+    assert(model.sharedClones[2].constant_scalar == 2)
+    assert(model.sharedClones[3].constant_scalar == 2)
+    assert(model.sharedClones[4] == nil)
     assert(model.sharedClones[5] == nil)
     assert(__.size(model.sharedClones) == 3)
 end
 
+
 local function test_load_save()
     local init = nn.MulConstant(1)
     local update = nn.MulConstant(2)
-    local model = nn.InitUpdateRecurrent(init, update, 1)
+    local model = nn.InitUpdateRecurrent(
+        init, update, nil --[[reinitialize_rate]], 1 --[[rho]])
 
     local a = {}
     for i = 1, 8 do a[i] = torch.rand(5, 5) end
@@ -104,6 +170,8 @@ local function test_load_save()
     f:seek(1)
 
     model = f:readObject()
+    model:clearState()
+    model:forget()
     local a_output_after_load = {}
     for i = 1, 8 do a_output_after_load[i] = model:forward(a[i]):clone() end
 
@@ -115,9 +183,13 @@ local function test_load_save()
     assert(test_util.almost_equals(a_output[6], a_output_after_load[6]))
     assert(test_util.almost_equals(a_output[7], a_output_after_load[7]))
     assert(test_util.almost_equals(a_output[8], a_output_after_load[8]))
+    model:forget()
 end
 
 test_util.run_test(test_init_only, 'Init only')
+test_util.run_test(test_reinit, 'Simple reinit')
 test_util.run_test(test_updates, 'Updates')
 test_util.run_test(test_check_sharedClones_table_sanity, 'sharedClones sanity')
+test_util.run_test(test_check_sharedClones_table_sanity_reinit,
+                   'sharedClones sanity with reinit')
 test_util.run_test(test_load_save, 'Test loading after saving')
