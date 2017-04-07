63404eb84f7c9f9600bda84073656f47e54ed043
Achal Dave <achalddave@live.com>
2017-04-07 15:24:43 -0400

    Allow counting Ignored and Reinitialized

--- scripts/one-off/count_reinit.py
+++ scripts/one-off/count_reinit.py
@@ -1,5 +1,6 @@
-from __future__ import division
+from __future__ import division, print_function
 import argparse
+import re
 
 if __name__ == "__main__":
     # Use first line of file docstring as description if a file docstring exists.
@@ -7,13 +8,26 @@ if __name__ == "__main__":
         description=__doc__.split('\n')[0] if __doc__ else '',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('evaluation_log')
+    parser.add_argument('keyword', default='Reinitialized', nargs='?')
 
     args = parser.parse_args()
-    num_init = 0
-    num_frames = 0
+    num_matched = 0
+    keyword = args.keyword
+    keyword_regex = re.compile(
+        '.*{} ([0-9]*) out of ([0-9]*) times.'.format(keyword))
+    num_frames_regex = re.compile('.*Finished [0-9]*/([0-9]*)')
+    num_frames = None
     with open(args.evaluation_log) as f:
         for line in f:
-            if not line.startswith('Reinitialized'): continue
-            num_init += int(line.split(' ')[1])
-            num_frames += int(line.split(' ')[4])
-    print(num_init, num_frames, num_init / num_frames)
+            if num_frames is None:
+                num_frames_match = num_frames_regex.match(line)
+                if num_frames_match:
+                    num_frames = int(num_frames_match.group(1))
+            match = keyword_regex.match(line)
+            if match:
+                num_matched += int(match.group(1))
+    print('{percent:.2f}% ({matched}/{total}) {keyword}'.format(
+        keyword=keyword.lower(),
+        matched=num_matched,
+        total=num_frames,
+        percent=100.0 * num_matched / num_frames))
