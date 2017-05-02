2427e094a4c85769b91f91f37110cba7e058bff9
Achal Dave <achalddave@live.com>
2017-05-02 10:59:58 -0400

    Convert --num_labels to number

--- scripts/batch_label_distribution.lua
+++ scripts/batch_label_distribution.lua
@@ -11,7 +11,7 @@ local parser = argparse() {
     description = 'Compute distribution of labels from samples.'
 }
 parser:argument('groundtruth_without_images_lmdb')
-parser:option('--num_labels'):default(65)
+parser:option('--num_labels'):default(65):convert(tonumber)
 parser:option('--num_samples'):default(100):convert(tonumber)
 parser:option('--background_weight'):default(20)
 parser:option('--output_counts')
